202ab4dc5f7eedc06ab25695193b064023ef36b5
katherynisabell <111913185+katherynisabell@users.noreply.github.com>
2022-12-04 14:47:35 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -22,3 +22,28 @@ function openPage(pageName, elmnt, color) {
 document.getElementById("defaultOpen").click();
 
 
+
+$(document).ready(function() {
+  $("select").on('change', function() {
+      $(this).find("option:selected").each(function() {
+          var selected = $(this).attr("value");
+          if (selected) {
+              $(".GFG").not("." + selected).hide();
+              $("." + selected).show();
+          } else {
+              $(".GFG").hide();
+          }
+
+      });
+  }).change();
+});
+
+$(function(){
+  $('#console-box').append('<br/>console is ready...');
+  
+  $('button').click(function(){
+    $('#console-box').prepend('<br>class:'+$(this).attr("class")+'|value:'+$(this).text()+'.');
+; 
+  });
+  
+});
